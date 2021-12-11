c4f0bd052dc65739947240d29df111f876e3448e
Ethan Kessel <eqkessel@gmail.com>
2021-12-11 01:01:59 -0500

    10: I forget to commit this for a day

---
+++ solutions/day10/day10.py
@@ -0,0 +1,143 @@
+# ADVENT OF CODE 2021
+# Challenge #10
+# Ethan Kessel
+
+SAMPLE_INPUT = """\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]\
+"""
+
+PT1_SAMPLE_ANS = 26397
+
+def pt1(input: str):
+    input = input.splitlines()
+
+    score = 0
+    points = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+    matching = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>',
+    }
+
+    for line in input:
+        parselist = []
+        for c in line:
+            if c in matching:
+                parselist.append(c)
+            else:
+                if parselist and c == matching[parselist.pop(-1)]:
+                    continue
+                else:
+                    score += points[c]
+                    break
+
+    return score
+
+PT2_SAMPLE_ANS = 288957
+
+def pt2(input: str):
+    input = input.splitlines()
+
+    scores = []
+    points = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    }
+    matching = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>',
+    }
+
+    for line in input:
+        parselist = []
+        for c in line:
+            if c in matching:
+                parselist.append(c)
+            else:
+                if c == matching[parselist.pop(-1)]:
+                    continue
+                else:
+                    parselist = []
+                    break
+        
+        if parselist:
+            score = 0
+            for c in parselist[::-1]:
+                score = score * 5 + points[matching[c]]
+            
+            scores.append(score)
+
+    scores.sort()
+
+    return scores[len(scores) // 2]
+
+# ==============================================================================
+
+import typing
+import time
+from os import path
+from colorama import init as colr_init, Fore, Back, Style
+
+# Runs the given solution with the input
+def run_with_input(func: typing.Callable[[str], typing.Any], input: str) -> tuple[typing.Any, float]:
+    start_time__ns = time.perf_counter_ns()
+    result = func(input)
+    end_time__ns = time.perf_counter_ns()
+    exec_time__us = (end_time__ns - start_time__ns) / 1000.0
+    return result, exec_time__us
+
+# Check result and ignore any errors with truth value of numpy arrays
+def check_result(result: typing.Any, expected: int):
+    try:
+        return bool(result == expected)
+    except:
+        return False
+
+if __name__ == "__main__":
+    colr_init()
+    print(f"{Back.WHITE}{Fore.BLACK} -* Advent of Code 2021 *- {Style.RESET_ALL}")
+    print("Challenge #10")
+    print("Ethan Kessel")
+    print(Style.DIM + "-" * 24 + Style.RESET_ALL)
+
+    pwd = path.dirname(__file__)
+    with open(path.join(pwd, "input.txt")) as input_file:
+        prob_input = input_file.read()
+        print(f"{Style.DIM}Loaded input file\n{input_file.name}{Style.NORMAL}")
+        print(Style.DIM + "-" * 24 + Style.RESET_ALL)
+
+        for part, soln_func, sample_result in zip((1,2), (pt1, pt2), (PT1_SAMPLE_ANS, PT2_SAMPLE_ANS)):
+            print(f"\nRunning part {part}...")
+
+            # Run and check sample
+            result, exec_time__us = run_with_input(soln_func, SAMPLE_INPUT)
+            print(f"Part {part} sample (exec time {exec_time__us:.1f}us): ", end="")
+            # Type comparison to short-circuit before invalid equality comparison
+            if check_result(result, sample_result):
+                print(f"{Back.GREEN}{Fore.BLACK} PASSED {Style.RESET_ALL}")
+
+                # Run full input
+                result, exec_time__us = run_with_input(soln_func, prob_input)
+                print(f"Part {part} (exec time {exec_time__us:.1f}us) result: {Style.BRIGHT}{result}{Style.RESET_ALL}")
+            else:
+                print(f"{Back.RED}{Fore.BLACK} FAILED {Style.RESET_ALL}")
+                print(f"Expected {sample_result}, produced:\n{Style.BRIGHT}{result}{Style.RESET_ALL}")
